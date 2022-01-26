c1c75d5152374f49e3d1fea3b587df191c0fa43f
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-01-26 22:59:12 +0530

    Add files via upload
    Commited.

---
+++ CoreJavaBrushup1.java
@@ -0,0 +1,21 @@
+
+public class CoreJavaBrushup1 {
+
+	public static void main(String[] args) {
+		
+		int Mynum=5;
+		String website = "www.udemy.com";
+		char letter = 'g';
+		double dec = 5.99;
+		boolean mycard = true;
+		
+		System.out.println(Mynum+" is the value stored in Mynum value");
+		System.out.println(website+" is the value stored in website value");
+		System.out.println(letter+" is the value stored in letter");
+		System.out.println(dec+" is the value stored in decimal value");
+		System.out.println(mycard+" is the value stored in boolean value");
+		
+
+	}
+
+}
