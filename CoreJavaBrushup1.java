9441d5791175acc5bf273e860619ca6fd4064732
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-01-27 23:21:08 +0530

    Add files via upload
    Commited.

--- CoreJavaBrushup1.java
+++ CoreJavaBrushup1.java
@@ -23,8 +23,19 @@ public class CoreJavaBrushup1 {
 		arr[3] = 4;
 		arr[4] = 5;
 		
-		int[] arr2 = {1,2,3,4,5};
+		int[] arr2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		System.out.println(arr[3]);
+		
+		//For loop
+		for(int i=0; i<arr.length; i++)  
+		{
+			System.out.println(arr[i]);
+		}
+		for(int i=0; i<arr2.length; i++)
+		{
+			System.out.println(arr2[i]);
+		}
+		
 		
 
 	}
