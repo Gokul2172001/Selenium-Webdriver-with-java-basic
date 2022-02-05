8701cb696ef8ca847b2604b80cc417007db2b68d
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-02-05 22:36:20 +0530

    Add files via upload
    Commited.

--- MethodsDemo2.java
+++ MethodsDemo2.java
@@ -1,14 +1,20 @@
 
 public class MethodsDemo2 {
 
-	public String getUserData() 
+public String getUserData() 
 	{
 		System.out.println ("hello world");
-		System.out.println("welcome to first demo of method ");		
-		return "Gokul E";
+		return "rahul shetty";
 		
 		
 
 	}
 
+}
+public class MethodDemo3 {
+	public String getUserData1()
+	{
+		String m= String("String are here to assign");
+		System.out.println(m);
+	}
 }
