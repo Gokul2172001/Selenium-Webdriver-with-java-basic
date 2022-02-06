c02b0bd90a25457ae447c30cfaad4ab12a949288
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-02-06 22:50:28 +0530

    Add files via upload
    Commited.

---
+++ Locators.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\Documents\\chromedriver_win32\\chromedriver.exe");  
+	    WebDriver driver=new ChromeDriver();
+	    driver.get("https://sso.teachable.com/secure/9521/identity/login");
+	    driver.findElement(By.id("email")).sendKeys("gokul.elumalai12@gmail.com");
+
+	}
+
+}
