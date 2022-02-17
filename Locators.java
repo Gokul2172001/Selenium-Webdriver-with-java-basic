1a1ca1cb9ecccf2f5575312034c47f046351cdd8
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-02-17 22:33:45 +0530

    Add files via upload
    Commited.

--- Locators.java
+++ Locators.java
@@ -12,16 +12,8 @@ public class Locators {
 	    driver.get("https://sso.teachable.com/secure/9521/identity/login");
 	    driver.findElement(By.id("email")).sendKeys("gokul.elumalai12@gmail.com");
 	    driver.findElement(By.name("password")).sendKeys("1341919");
-	    //driver.FindElement(By.className("btn-primary button p-v-2-xs p-h-5-xs m-v-3-xs col-12-xs")).click();
-		css=input[id$='mail'] 
-		WebElement Email = driver.findElement(By.cssSelector("input[id=email]"));
-		Email.SendKeys("hello@sampleemail.com");
-		driver.findElement(By.id("forgot password")).click();
-		driver.findElement(By.id("sign up")).click();
-		if(driver.findElement(By.id("help & support")).click();
-		{
-			System.out.println(checkbox=click 
-		}
+		driver.findElement(By.className("SigninBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		
 
 	}
