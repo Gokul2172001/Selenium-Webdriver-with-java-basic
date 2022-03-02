e8e57bc685c76214ca40cf1b459d2f22cb958c75
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-03-02 22:34:53 +0530

    Add files via upload
    Commited.

--- Locators.java
+++ Locators.java
@@ -18,7 +18,7 @@ public class Locators {
 		driver.findElement(By.LinkText("forget password")).click();
 		driver.findElement(By.id("phone")).sendkeys("9566554241");
 		driver.findElement(By.id("email")).sendkeys("gokul.elumalai12@gmail.com");
-		driver.findElement(By.id("send code")).click();
+		System.out.println(driver.findElement(By.id("send code")).click());
 		driver.findElement(By.id("enter the code")).sendkeys("1111111");
 		driver.findElement(By.css("Enter your new password")).sendkeys("gokul114"):
 		driver.findElement(By.css("Re enter your new password")).sendkeys("gokul114"):
