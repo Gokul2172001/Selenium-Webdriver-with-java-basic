7335404b916ddc3e92b787b6fe10338820d511c5
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-03-04 22:46:23 +0530

    Add files via upload
    Commited.

--- Locators.java
+++ Locators.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args)		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\Documents\\chromedriver_win32\\chromedriver.exe");  
 	    WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitWait(Duration.ofSeconds(15));
