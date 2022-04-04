d0e5189a06d94845f8ca4b2646e188952d72d7a8
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-04-04 23:48:30 +0530

    Add files via upload
    Commited.

--- Locators.java
+++ Locators.java
@@ -13,19 +13,19 @@ public class Locators {
 	    driver.findElement(By.name("password")).sendKeys("1341919");
 		driver.findElement(By.className("SigninBtn")).click();
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-		//driver.findElement(By.LinkText("forget password")).click();
-		//driver.findElement(By.id("phone")).sendkeys("9566554241");
-		//driver.findElement(By.id("email")).sendkeys("gokul.elumalai12@gmail.com");
-		//System.out.println(driver.findElement(By.id("send code")).click());
-		//System.out.println(driver.out.println(driver.find Element(By.css("check the email for confirmation"));
-		//driver.findElement(By.id("enter the code")).sendkeys("1111111");
-		//driver.findElement(By.css("Enter your new password")).sendkeys("gokul114"):
-		//driver.findElement(By.css("Re enter your new password")).sendkeys("gokul114"):
-		//driver.findElement(By.css("change")).click():
-		//System.out.println(driver(By.xpath("Your password has been changed"));
-		//driver.findElement(driver(By.id("Login again").click();
-		//driver.findElement(By.xpath("//input[@placeholder='name']"));
-		//end();
+		driver.findElement(By.LinkText("forget password")).click();
+		driver.findElement(By.id("phone")).sendkeys("9566554241");
+		driver.findElement(By.id("email")).sendkeys("gokul.elumalai12@gmail.com");
+		System.out.println(driver.findElement(By.id("send code")).click());
+		System.out.println(driver.out.println(driver.find Element(By.css("check the email for confirmation"));
+		driver.findElement(By.id("enter the code")).sendkeys("1111111");
+		driver.findElement(By.css("Enter your new password")).sendkeys("gokul114"):
+		driver.findElement(By.css("Re enter your new password")).sendkeys("gokul114"):
+		driver.findElement(By.css("change")).click():
+		System.out.println(driver(By.xpath("Your password has been changed"));
+		driver.findElement(driver(By.id("Login again").click();
+		driver.findElement(By.xpath("//input[@placeholder='name']"));
+		end();
 
 	}
 
