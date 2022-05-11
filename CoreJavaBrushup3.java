4caa0ab94d0115d470358ec5f8ec17276cade82c
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-05-11 21:52:27 +0530

    Commited.

--- CoreJavaBrushup3.java
+++ CoreJavaBrushup3.java
@@ -3,24 +3,9 @@ public class CoreJavaBrushUp3 {
 
 
 public static void main(String[] args) {
-
-// TODO Auto-generated method stub
-
-
-
-//string is an object //String literal
-
-
-
  String s1 = "Rahul Shetty Academy";
 
 String s5 = "hello";
-
-
-
-
-
-//new
 
 String s2 = new String("Welcome");
 
