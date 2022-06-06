5f0c6845e47b9f91d3e1b700f08c63f6ae35e8ca
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-06-06 23:49:47 +0530

    Commited.

--- Example_of_parameterized_constructor.java
+++ Example_of_parameterized_constructor.java
@@ -10,7 +10,6 @@ public class Example_of_parameterized_constructor {
 		System.out.println(id+" "+name);
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Example_of_parameterized_constructor s1=new Example_of_parameterized_constructor(111,"Karan");
 		Example_of_parameterized_constructor s2=new Example_of_parameterized_constructor(222,"Aryan");
 		s1.display();
