c2f138dc7be8e452f3a424ef7b8a2f1b5c756de1
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2022-06-28 22:32:50 +0530

    Committed.

--- Locators1.java
+++ Locators1.java
@@ -6,7 +6,7 @@ public class Locators1 {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\Documents\\chromedriver_win32\\chromedriver.exe");  
 	    WebDriver driver=new ChromeDriver();
-		//driver.manage().timeouts().implicitWait(Duration.ofSeconds(20);
+		driver.manage().timeouts().implicitWait(Duration.ofSeconds(20);
 	    driver.get("https:terv.pro/login");
 	    driver.findElement(By.name("Login")).click();
 
