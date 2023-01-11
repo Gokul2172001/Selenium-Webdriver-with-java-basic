4a57a1eb8ab00160ef89769a89f8f0150b1a5141
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-01-11 21:35:59 +0530

    Update UpdatedDropdown.java

--- UpdatedDropdown.java
+++ UpdatedDropdown.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 public class UpdatedDropdown {
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ELCOT\\Documents\\chromedriver_win32(1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); //URL in the browser
