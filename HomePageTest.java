749a4ecfd6989ab1a131b9083238044640a46846
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-01-16 23:53:55 +0530

    First Commit.

---
+++ HomePageTest.java
@@ -0,0 +1,46 @@
+package com.crm.qa.testcases;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class HomePageTest extends TestBase {
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	public HomePageTest() {
+		super();
+	}
+	
+
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@Test
+	public void verifyHomePageTitleTest() {
+		String homePageTitle = homePage.verifyHomePageTitle();
+		Assert.assertEquals(homePageTitle, "My Account","Title not matched");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
