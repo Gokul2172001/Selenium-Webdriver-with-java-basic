2d9b83f50bb6d67eb1ed921346acaef885fedb88
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-02-24 23:40:39 +0530

    Update UpdatedDropdown.java

--- UpdatedDropdown.java
+++ UpdatedDropdown.java
@@ -9,12 +9,6 @@ public class UpdatedDropdown {
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); //URL in the browser
 		driver.findElement(By.id("divpaxinfo")).click();
 		   Thread.sleep(2000L);
-		   /*int i=1;
-		while(i<5)
-		{
-		driver.findElement(By.id("hrefIncAdt")).click();//4 times
-		i++;
-		}*/
 		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 		for(int i=1;i<5;i++)
 		{
