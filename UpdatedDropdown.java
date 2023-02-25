995d31cc6b1166d0cc2e3008873d5897ad9812a9
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-02-25 23:22:32 +0530

    Update UpdatedDropdown.java

--- UpdatedDropdown.java
+++ UpdatedDropdown.java
@@ -2,19 +2,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+
 public class UpdatedDropdown {
 	public static void main(String[] args) throws InterruptedException {
+		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ELCOT\\Documents\\chromedriver_win32(1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); //URL in the browser
 		driver.findElement(By.id("divpaxinfo")).click();
 		   Thread.sleep(2000L);
 		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
 		for(int i=1;i<5;i++)
 		{
 		driver.findElement(By.id("hrefIncAdt")).click();
 		}
 		driver.findElement(By.id("btnclosepaxoption")).click();
+		
 		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	}
