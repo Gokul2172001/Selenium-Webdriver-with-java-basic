bbc359ccf682a0ecaf237dfc35befaf53fb1b561
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-02-27 23:27:29 +0530

    Update UpdatedDropdown.java

--- UpdatedDropdown.java
+++ UpdatedDropdown.java
@@ -8,17 +8,15 @@ public class UpdatedDropdown {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ELCOT\\Documents\\chromedriver_win32(1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); //URL in the browser
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); 
 		driver.findElement(By.id("divpaxinfo")).click();
 		   Thread.sleep(2000L);
 		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
-		
 		for(int i=1;i<5;i++)
 		{
 		driver.findElement(By.id("hrefIncAdt")).click();
 		}
 		driver.findElement(By.id("btnclosepaxoption")).click();
-		
 		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	}
